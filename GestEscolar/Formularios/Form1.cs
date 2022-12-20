b5327d36a7df760fffb1eb75347ddae47e9490ad
Jozenartes <jozenarts12@gmail.com>
2022-12-20 19:49:41 +0100

    Codigo de Limpeza

--- GestEscolar/Formularios/Form1.cs
+++ GestEscolar/Formularios/Form1.cs
@@ -14,6 +14,21 @@ namespace Formularios
 {
     public partial class Form1 : Form
     {
+        ClasseNegocio clsUser = new ClasseNegocio();
+        ClasseEntidade clsEnt = new ClasseEntidade();
+
+        public static string usuario_nome, //nome do usuario
+                             id_tipo, //tipo do usuario a logar
+                             usuario_geral, 
+                             usuario_codigo;
+
+        FrmPrincipal f = new FrmPrincipal();
+
+        private void Limpar()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
         public Form1()
         {
             InitializeComponent();
