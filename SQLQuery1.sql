8d247684286d8433732afb5bca513c42bea8bb73
Jozenartes <jozenarts12@gmail.com>
2022-12-21 13:32:36 +0100

    Database files

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+create database escola
+go
+use escola
+go
+
+create table usuario
+(
+id_usuario varchar(5) not null,
+nome_usuario varchar(20)not null,
+senha_usuario varchar(10) not null,
+id_tipo varchar(15) not null,
+)
+
+create table Tipo
+(
+id_tipo varchar(5) not null,
+nome_tipo varchar(20)not null,
+)
