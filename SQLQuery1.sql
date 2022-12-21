5525ab7d497fd34f4ddbaa8fcb38d113f5a5db55
Jozenartes <jozenarts12@gmail.com>
2022-12-21 16:59:05 +0100

    added elements

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,9 +10,12 @@ nome_usuario varchar(20)not null,
 senha_usuario varchar(10) not null,
 id_tipo varchar(15) not null,
 )
+insert into usuario values('00001','admin','admin','Administrador') /*user added*/
 
 create table Tipo
 (
 id_tipo varchar(5) not null,
 nome_tipo varchar(20)not null,
 )
+
+insert into Tipo values('T0001','Administrador') /*Tipo de usuario, Admin*/
