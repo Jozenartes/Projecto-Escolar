7f404d308e2c6bea84a421890157a6f749d0177f
Jozenartes <jozenarts12@gmail.com>
2022-12-24 20:44:31 +0100

    codigo de Login

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,10 +10,15 @@ nome_usuario varchar(20)not null,
 senha_usuario varchar(10) not null,
 id_tipo varchar(15) not null,
 )
-
+insert into usuario values('00001','admin','admin','Administrador')
 
 create table Tipo
 (
 id_tipo varchar(5) not null,
 nome_tipo varchar(20)not null,
 )
+insert into Tipo values('T0001','Administrador') /*T de tipo*/
+
+create proc sp_logar
+@usuario varchar(20),
+@senha varchar(10)
