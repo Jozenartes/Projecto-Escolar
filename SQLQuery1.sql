6624aa75b94c6c4c5bc361b613ce077c623f1bdf
Jozenartes <jozenarts12@gmail.com>
2022-12-24 20:36:36 +0100

    new

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,16 +10,10 @@ nome_usuario varchar(20)not null,
 senha_usuario varchar(10) not null,
 id_tipo varchar(15) not null,
 )
-insert into usuario values('00001','admin','admin','Administrador') /*user added*/
+
 
 create table Tipo
 (
 id_tipo varchar(5) not null,
 nome_tipo varchar(20)not null,
 )
-
-insert into Tipo values('T0001','Administrador') /*Tipo de usuario, Admin*/
-
-create proc sp_logar
-@usuario varchar(20),
-@senha varchar(10)
