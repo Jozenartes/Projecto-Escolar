6fbeb625369fba68d179f38da4ef8e176371603c
Jozenartes <jozenarts12@gmail.com>
2022-12-27 14:18:45 +0100

    ClasseDados e Table
    Adicionei uma classedados e uma mesa(table) DataTable N_Login

--- GestEscolar/CapaDeNegocio/ClasseNegocio.cs
+++ GestEscolar/CapaDeNegocio/ClasseNegocio.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CapaDeDados;
-using CapaDeEntidade;
+using CapaDeDados;//
+using CapaDeEntidade;//
+using System.Data; //libary for DataTable
 
 namespace CapaDeNegocio
 {
     public class ClasseNegocio
     {
+        ClasseDados clsDados = new ClasseDados();
+        public DataTable N_Login(ClasseEntidade obje)
+        {
+            return clsdados.DLogin(obje);
+        }
     }
 }
