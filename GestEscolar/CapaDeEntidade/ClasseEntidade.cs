1aae04a4d5d279504a633d1001deeac57966aea2
Jozenartes <jozenarts12@gmail.com>
2023-01-11 17:08:44 +0100

    Changes to new class

--- GestEscolar/CapaDeEntidade/ClasseEntidade.cs
+++ GestEscolar/CapaDeEntidade/ClasseEntidade.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Data;
-
+using CapaDeNegocios;
 namespace CapaDeEntidade
 {
     public class ClasseEntidade
     {
         public string usuario { get; set; }
         public string senha { get; set; }
+
+        public DataTable N_Login(global::CapaDeNegocios.ClasseNegocio clsUser)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- GestEscolar/CapaDeNegocios/ClasseNegocio.cs
+++ GestEscolar/CapaDeNegocios/ClasseNegocio.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace CapaDeNegocios
 {
-    public class Class1
+    public class ClasseNegocio
     {
+
+        public DataTable N_Login(ClasseEntidade obje)
+        {
+            return clsDados.DLogin(obje);
+        }
     }
 }
